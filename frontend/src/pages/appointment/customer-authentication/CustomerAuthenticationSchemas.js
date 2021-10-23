8cc04888fb371fa4353c05ee1cd34b75c16995e7
NetanelV <emailofnetanel@gmail.com>
2021-10-23 22:13:01 +0300

    Change file name

--- frontend/src/pages/appointment/customer-authentication/CustomerAuthenticationSchemas.js
+++ frontend/src/pages/appointment/customer-authentication/CustomerAuthenticationSchemas.js
@@ -1,17 +1,17 @@
-import * as yup from 'yup';
-import { phoneNumberPattern } from '../../../libs/utils/regex-patterns';
+import * as yup from "yup";
+import { phoneNumberPattern } from "../../../libs/utils/regex-patterns";
 
 export const customerDetailsSchema = yup.object().shape({
   validationCode: yup
     .string()
-    .min(4, 'הקוד צריך להכיל 4 ספרות')
-    .max(4, 'הקוד צריך להכיל 4 ספרות')
-    .required('שדה חובה'),
+    .min(4, "הקוד צריך להכיל 4 ספרות")
+    .max(4, "הקוד צריך להכיל 4 ספרות")
+    .required("שדה חובה"),
 });
 
 export const customerAuthenticationSchema = yup.object().shape({
   customerPhone: yup
     .string()
-    .required('שדה חובה')
-    .matches(phoneNumberPattern, 'טלפון לא חוקי'),
+    .required("שדה חובה")
+    .matches(phoneNumberPattern, "טלפון לא חוקי"),
 });
